44dfb6a9da269ab7ce4232f18ce8caf616514537
David Awad <Davidawad64@gmail.com>
2015-10-01 18:33:35 -0400

    Update goldbach.py

--- goldbach.py
+++ goldbach.py
@@ -76,23 +76,6 @@ def even_number(num):
     else:
         return False
 
-'''
-def is_prime(num):
-    """
-    Returns True if the number is prime
-    else False.
-    """
-    if num == 0 or num == 1:
-        return False
-    for x in range(2, num):
-        if num % x == 0:
-            return False
-    else:
-        print(str(num) + " is a prime number")
-        return True
-'''
-
-
 def is_prime(list_primes, num):
     '''
         return if a number is prime through binary search - O(log(n))
